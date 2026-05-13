0c1087b507c0fe8bf8f55b3a6c4a8b04be097235
Gowtham4567-oss <gowtha4567@gmail.com>
2026-05-12 17:30:58 -0700

    Add basic calculator functions in script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const display = document.getElementById('display');
 
+// Append numbers or operators to the screen
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+// Clear the entire screen
+function clearDisplay() {
+    display.value = "";
+}
+
+// Delete the last character entered
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+// Perform the calculation
+function calculate() {
+    try {
+        // eval() takes the string and treats it as math
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
